5257e8f114a60ac4d100c83674c08e665ca2b2d5
Gavin <gavinmischler@gmail.com>
2018-05-22 00:38:50 -0400

    interface is working

--- main.c
+++ main.c
@@ -1,11 +1,28 @@
 #include "interface.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 int main() {
-  char user_menu_input[50];
-  print_menu();
-  scanf("%s\n", user_menu_input);
-  printf("%s\n", user_menu_input);
+  char user_menu_input[20];
+  char user_input = 'a';
+  while (user_input != 'q') {
+    print_menu();
+    scanf("%s", user_menu_input);
+    printf("user inputted %s\n", user_menu_input);
+    user_input = user_menu_input[0];
+    if (user_input == 'q') {
+      // do nothing
+    } else if (strlen(user_menu_input) != 1 || (user_input != 'n' && user_input != 'd' && user_input != 'e' && user_input != 'c' && user_input != 'l' && user_input != 's' && user_input != 'q')) {
+      bad_menu_option(user_input);
+    } else {
+      printf("length is %lu and the char is %c\n", strlen(user_menu_input), user_input);
+
+
+    }
+
+  }
+
   return 0;
 }
