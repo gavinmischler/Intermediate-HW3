dbf0f7b17081c4c0b5466cd80acb196557d399e3
Gavin <gavinmischler@gmail.com>
2018-05-21 23:47:10 -0400

    next commit but interface.h can't be found

--- main.c
+++ main.c
@@ -1,2 +1,11 @@
-#include <interface.h>
+#include "interface.h"
 #include <stdio.h>
+
+
+int main() {
+  char user_menu_input[50];
+  print_menu();
+  scanf("%s\n", user_menu_input);
+  printf("%s\n", user_menu_input);
+  return 0;
+}
